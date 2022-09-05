b4536e4894adb2dba7f65fe629b32fb6d9d0221c
asahifuke <asahi.fuke@medpeer.co.jp>
2022-09-05 15:18:58 +0900

    eslintを通しました

--- TodoList.js
+++ TodoList.js
@@ -1,60 +1,60 @@
 Vue.createApp({
-  data() {
+  data () {
     return {
       todos: []
-    };
+    }
   },
   created: function () {
-    this.all();
+    this.all()
   },
 
   methods: {
-    all() {
-      const todos = [];
-      for(let i = 0 ; i < localStorage.length ; i++) {
-        const key = localStorage.key(i);
-        const todo = JSON.parse(localStorage.getItem(key));
-        todo['id'] = key;
-        todos.push(todo);
+    all () {
+      const todos = []
+      for (let i = 0; i < localStorage.length; i++) {
+        const key = localStorage.key(i)
+        const todo = JSON.parse(localStorage.getItem(key))
+        todo.id = key
+        todos.push(todo)
       }
-      this.todos = todos;
+      this.todos = todos
     },
 
-    $_find(todo) {
-      return JSON.parse(localStorage.getItem(todo.id));
+    $_find (todo) {
+      return JSON.parse(localStorage.getItem(todo.id))
     },
 
-    create() {
+    create () {
       const todo = {
-        title: this.title, 
+        title: this.title,
         isEditing: false
-      };
-      localStorage.setItem(Date.now().toString(36) + Math.random().toString(36),  JSON.stringify(todo));
-      this.all();
+      }
+      localStorage.setItem(Date.now().toString(36) + Math.random().toString(36), JSON.stringify(todo))
+      this.all()
     },
 
-    edit(todo) {
-      const editingTodo = this.$_find(todo);
-      editingTodo['isEditing'] = true;
-      localStorage.setItem(todo.id, JSON.stringify(editingTodo));
-      this.all();
+    edit (todo) {
+      const editingTodo = this.$_find(todo)
+      editingTodo.isEditing = true
+      localStorage.setItem(todo.id, JSON.stringify(editingTodo))
+      this.all()
     },
 
-    update(todo) {
-      const editingTodo = this.$_find(todo);
-      editingTodo['isEditing'] = false;
-      editingTodo['title'] = todo.title;
-      localStorage.setItem(todo.id, JSON.stringify(editingTodo));
-      this.all();
+    update (todo) {
+      const editingTodo = this.$_find(todo)
+      editingTodo.isEditing = false
+      editingTodo.title = todo.title
+      localStorage.setItem(todo.id, JSON.stringify(editingTodo))
+      this.all()
     },
 
-    destroy(todo) {
-      localStorage.removeItem(todo.id);
-      this.all();
+    destroy (todo) {
+      localStorage.removeItem(todo.id)
+      this.all()
     },
-  
-    isEditing(todo) {
-      return todo.isEditing;
-    },
+
+    isEditing (todo) {
+      return todo.isEditing
+    }
   }
-}).mount('#app');
+}).mount('#app')
